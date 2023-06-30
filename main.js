bda07067d7879dedf0542628a3117c3d50d66247
+ <+>
2023-06-30 16:13:19 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,6 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likes =  document.querySelectorAll('.like')
+likes.forEach(like => {
+  like.addEventListener('click', () => {mimicServerCall})
+})
+.then(() => {
+    toggleLike(button);
+  })
+  .catch((error) => {
+    displayError(error)
+})
+function toggleLike(button) {
+  const heart = button.querySelector('.like-glyph');
+  if (heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART;
+    heart.classList.add('activated-heart');
+  } else {
+    heart.innerText = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
+
+// Display error functionality
+function displayError(error) {
+  errorMessage.innerText = error;
+  errorModal.classList.remove('hidden');
+  setTimeout(() => {
+    errorModal.classList.add('hidden');
+  }, 3000);
+}
 
 
 
